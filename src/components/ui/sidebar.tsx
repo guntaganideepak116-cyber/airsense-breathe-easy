efbd748ed6b034851e939cd0dfae509a73c9b770
guntaganideepak116-cyber <guntaganideepak116@gmail.com>
2026-08-21 13:17:41 +0530

    refactor(ui): simplify Tailwind data attribute variants in Sidebar component

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -318,11 +318,11 @@ const SidebarRail = React.forwardRef<HTMLButtonElement, React.ComponentProps<"bu
         title="Toggle Sidebar"
         className={cn(
           "absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear after:absolute after:inset-y-0 after:left-1/2 after:w-0.5 hover:after:bg-sidebar-border group-data-[side=left]:-right-4 group-data-[side=right]:left-0 sm:flex",
-          "[&[data-side=left]]:cursor-w-resize [&[data-side=right]]:cursor-e-resize",
-          "[&[data-side=left][data-state=collapsed]]:cursor-e-resize [&[data-side=right][data-state=collapsed]]:cursor-w-resize",
+          "data-[side=left]:cursor-w-resize data-[side=right]:cursor-e-resize",
+          "data-[side=left]:data-[state=collapsed]:cursor-e-resize data-[side=right]:data-[state=collapsed]:cursor-w-resize",
           "group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full group-data-[collapsible=offcanvas]:hover:bg-sidebar",
-          "[&[data-side=left][data-collapsible=offcanvas]]:-right-2",
-          "[&[data-side=right][data-collapsible=offcanvas]]:-left-2",
+          "data-[side=left]:data-[collapsible=offcanvas]:-right-2",
+          "data-[side=right]:data-[collapsible=offcanvas]:-left-2",
           className,
         )}
         {...props}
